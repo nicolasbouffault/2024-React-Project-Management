d1ca0058b45c5ffad691e725cadce0da9cdf735b
Nicolas Bouffault <nicolasbouffault@gmail.com>
2024-02-03 14:25:55 +0000

    Styling sidebar and no projects screen

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,27 @@ import React from "react";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
-    const handleAddProject = () => {
-        console.log("hey");
-    };
+  const handleAddProject = () => {
+    console.log("hey");
+  };
 
-    return (
-        <div className="flex">
-            <ProjectsSidebar onAddProject={handleAddProject} />
-        </div>
-    );
+  return (
+    <div className="flex project-app">
+      <ProjectsSidebar onAddProject={handleAddProject} />
+      <div className="w-4/5 flex flex-col text-center items-center main-content pt-44">
+        <img className="w-16" src="/src/assets/no-projects.png" alt="" />
+        <p className="text-black/50 font-medium">
+          Select a project or get started with a new one
+        </p>
+        <button
+          className="btn rounded-lg bg-black text-white/50 hover:text-white my-8 py-2 px-4 relative"
+          onClick={handleAddProject}
+        >
+          Create new project
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -1,18 +1,33 @@
 import React from "react";
 
 export default function ProjectsSidebar({ onAddProject }) {
-    return (
-        <div className="sidebar w-1/5 h-screen rounded-tr-xl px-10 pt-16 pb-10 bg-black text-white">
-            <h1 className="uppercase font-medium text-lg">Your Projects</h1>
-            <button
-                className="btn rounded-lg bg-white/20"
-                onClick={onAddProject}
-            >
-                Add Project
-            </button>
-            <ul>
-                <li>Project 1</li>
-            </ul>
-        </div>
-    );
+  return (
+    <div className="sidebar w-1/5 h-screen rounded-tr-xl px-10 pt-16 pb-10 mt-10 bg-black text-white">
+      <h1 className="uppercase font-medium text-lg">Your Projects</h1>
+      <button
+        className="btn rounded-lg bg-white/20 text-white/40 hover:bg-white/30 hover:text-white my-8 py-2 px-4 relative"
+        onClick={onAddProject}
+      >
+        <span className="pr-1">+</span>Add Project
+      </button>
+      <ul className="flex flex-col gap-2">
+        <li>
+          <a
+            className="block hover:text-white hover:bg-white/10 text-white/40 py-1 px-2"
+            href=""
+          >
+            Project 1
+          </a>
+        </li>
+        <li>
+          <a
+            className="block hover:text-white hover:bg-white/10 text-white/40 py-1 px-2"
+            href=""
+          >
+            Project 2
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 }
